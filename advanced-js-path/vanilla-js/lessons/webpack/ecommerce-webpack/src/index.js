6c8247f9459400a345ee3e71f7193488e7612d69
chadelz-dev <133863404+chadelz-dev@users.noreply.github.com>
2025-05-08 14:24:17 +0200

    Added CSS and HTML to ecommerce-webpack

--- advanced-js-path/vanilla-js/lessons/webpack/ecommerce-webpack/src/index.js
+++ advanced-js-path/vanilla-js/lessons/webpack/ecommerce-webpack/src/index.js
@@ -1,4 +1,17 @@
+// import css to style the card
+import './styles.css';
+
 // Imports product data and logs it
 import { productData } from './product.js';
 
-console.log(`Product: ${productData.name}, Price: $${productData.price}`);
+// creates a product card element
+const cardElement = document.createElement('div');
+cardElement.classList.add('card'); // adds a "card" class to the element
+
+cardElement.innerHTML = `
+    <h2>${productData.name}</h2>
+    <p>Price: $${productData.price}</p>
+`;
+
+// add the card to the page
+document.getElementById('card-container').appendChild(cardElement);
--- advanced-js-path/vanilla-js/lessons/webpack/ecommerce-webpack/webpack.config.js
+++ advanced-js-path/vanilla-js/lessons/webpack/ecommerce-webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path'); // Helps Webpack find the dist/ folder
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development', // Makes the bundle readable for debugging
@@ -7,5 +8,18 @@ module.exports = {
     filename: 'main.js', // Name of the bundled file
     path: path.resolve(__dirname, 'dist'), // Puts it in dist/
     clean: true, // Clears dist/ before each build
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html', // Copies this to dist/index.html
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // find the css files
+        use: ['style-loader', 'css-loader'], // processes the CSS files
+      },
+    ],
   },
 };
